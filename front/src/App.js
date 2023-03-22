7288902e6f351f93f51420e030a85cc8f17734b8
Peannut <zakariaspam0@gmail.com>
2023-03-22 20:03:16 +0000

    lkhadma dl front hh

--- front/src/App.js
+++ front/src/App.js
@@ -1,34 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function MyLoginButton()
 {
   return (
-    
-    <button>
-      lOGIN
-    </button>
+    <div>
+      <button>
+        lOGIN
+      </button>
+    </div>
   )
 }
+// function Handleclick()
+// {
+//   const [type,Setatus] = useState("text")
+//   Setatus(type === "password" ? "text" : "password");
+// }
 
 
 function MyInputField()
 {
+  // const [type,Setatus] = useState("text")
+  // useEffect(() => {
+  //   type = "text" ? "Password": "text"
+  // }, [type]);
+  const [type, setType] = useState("password");
+
+  function handleButtonClick() {
+    setType(type === "password" ? "text" : "password");
+  }
+
   return (
-    <label>
-      Your email:
-      <input name="inputmail"/>
-    </label>
+    <div>
+      <label>
+        Your email:
+        <input name="inputmail"/>
+      </label>
+      <div>
+        <label>
+          Your password:
+          <input name="inputmail" type={type}/>
+          <button onClick={handleButtonClick}> bi9</button>
+        </label>
+      </div>
+    </div>
   )
 }
 
 function App() {
   return (
     <div className="App">
-      <MyLoginButton/>
       
       <MyInputField/>
+      <MyLoginButton/>
     </div>
   );
 }
