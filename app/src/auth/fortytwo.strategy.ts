5330de41fcc96fcb5663d30ae38ce61121d5a2ad
Anas Amoussaoui <aamoussa@e2r11p6.1337.ma>
2023-03-22 19:03:04 +0000

    saving

--- app/src/auth/fortytwo.strategy.ts
+++ app/src/auth/fortytwo.strategy.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-42';
-import * as dotenv from 'dotenv';
+
+
 
 @Injectable()
 export class FortyTwoStrategy extends PassportStrategy(Strategy, '42') {
   constructor() {
     super({
-      clientID: 'u-s4t2ud-1f5ab0b02b6fb3c9a33bb8a7cba4ded3d1afb8d9de0039708408694fc72d2f8d',
-      clientSecret: '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',
-      callbackURL: 'https://ereur-fuzzy-invention-j99p4g9vwqrc5rgj-5000.preview.app.github.dev/auth/42/callback',
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: 'http://localhost:5000/auth/42/callback',
       scope: 'public',
     });
   }
